3525dfad989facabf75dee7e01f4c12b1c32e2b4
Carlos Ramirez <carlosrv125@icloud.com>
2020-05-06 05:41:52 +0800

    create finished

--- script.sh
+++ script.sh
@@ -9,8 +9,8 @@ terraform apply
 gcloud config set compute/zone $(terraform output location)
 gcloud container clusters get-credentials $(terraform output cluster_name)
 gcloud auth configure-docker
-docker pull carlosrv999/test-python:latest
-docker tag carlosrv999/test-python:latest gcr.io/$(terraform output project)/pytest:v2
+docker build -t newbuild:latest app/
+docker tag newbuild:latest gcr.io/$(terraform output project)/pytest:v2
 docker push gcr.io/$(terraform output project)/pytest:v2
 export IMAGE_URL='gcr.io/'"$project"'/pytest:v2'
 envsubst < manifests/deployment.yaml > manifests/deployment-subst.yaml
