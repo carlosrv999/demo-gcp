30f7a9855740127fa23c8bd4b111919e85cc8bf5
Carlos Ramirez <carlosrv125@icloud.com>
2020-05-06 03:47:09 +0800

    create finished

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #terraform apply
 source ./email.txt
 source ./terraform.tfvars
-echo $project
-echo $region
-echo $zone
 gcloud auth activate-service-account ${EMAIL} --key-file=credentials.json
 gcloud config set project ${project}
 gcloud services enable container.googleapis.com
@@ -13,3 +10,9 @@ gcloud container clusters get-credentials $(terraform output cluster_name)
 docker pull carlosrv999/test-python:latest
 docker tag carlosrv999/test-python:latest gcr.io/$(terraform output project)/pytest:v2
 docker push gcr.io/$(terraform output project)/pytest:v2
+export IMAGE_URL='gcr.io/'"$project"'/pytest:v2'
+envsubst < manifests/deployment.yaml > manifests/deployment-subst.yaml
+kubectl apply -f manifests/deployment-subst.yaml
+kubectl apply -f manifests/service.yaml
+kubectl apply -f manifests/ingress.yaml
+rm -f manifests/deployment-subst.yaml
